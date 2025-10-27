325d2c5f333453280814ccc105a19ffc5a17f696
dileepskb <dileepskb350@gmail.com>
2025-10-27 21:25:41 +0530

    add new add carousel

---
+++ app/_components/dummydata/DummyData.ts
@@ -0,0 +1,304 @@
+export const ProductsDummy = [
+  {
+    id: 1,
+    title: "Flyers",
+    discription:
+      "Are you ready to elevate your marketing game and capture your audience’s attention with impactful promotional materials?",
+    additionalDescription: [
+      {
+        name: "",
+        value: "",
+      },
+    ],
+    reviews: 15,
+    price: 20,
+    discountedPrice: "Upto 40% Off",
+    imgs: {
+      thumbnails: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Flyer-Image-350-x-350.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/business-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Political-Updated-Images-900-X-500-1.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Sport-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-Flyer-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Emergency-Awarness-Flyers.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Health-Flyer-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Restaurent-New-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-E-State-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Automative-Flyer-new.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/New-Photography-Flyer900-x-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Event-Party.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Flyers-tech.81.jpg",
+      ],
+      previews: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Flyer-Image-350-x-350.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/business-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Political-Updated-Images-900-X-500-1.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Sport-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-Flyer-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Emergency-Awarness-Flyers.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Health-Flyer-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Restaurent-New-flyer-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-E-State-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Automative-Flyer-new.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/New-Photography-Flyer900-x-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Event-Party.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Flyers-tech.81.jpg",
+      ],
+    },
+    productOption: [
+      {
+        name: "Size",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 20,
+            discount: 0,
+          },
+          {
+            label: "10X20",
+            value: 25,
+            discount: 0,
+          },
+        ],
+      },
+      {
+        name: "Quantity",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 1,
+            discount: 0, //percent
+          },
+          {
+            label: "A4",
+            value: 10,
+            discount: 1, //percent
+          },
+          {
+            label: "10X20",
+            value: 20,
+            discount: 20, //percent
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Door Hangers",
+    discription:
+      "Are you ready to elevate your marketing game and capture your audience’s attention with impactful promotional materials?",
+    additionalDescription: [
+      {
+        name: "",
+        value: "",
+      },
+    ],
+    reviews: 15,
+    price: 20,
+    discountedPrice: "Upto 40% Off",
+    imgs: {
+      thumbnails: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Door-Hanger-350-X-350.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Automative-Door-Hanger-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Business-essential-900-X-500-new.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Door-Hanger-900-X-500-1.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Political-Door-Hanger-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-Estate-Door-Hanger.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Restaurant-Door-Hanger.jpg",
+      ],
+      previews: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Door-Hanger-350-X-350.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Automative-Door-Hanger-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Business-essential-900-X-500-new.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Door-Hanger-900-X-500-1.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Political-Door-Hanger-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-Estate-Door-Hanger.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Restaurant-Door-Hanger.jpg",
+      ],
+    },
+    productOption: [
+      {
+        name: "Size",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 20,
+            discount: 0,
+          },
+          {
+            label: "10X20",
+            value: 25,
+            discount: 0,
+          },
+        ],
+      },
+      {
+        name: "Quantity",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 1,
+            discount: 0, //percent
+          },
+          {
+            label: "A4",
+            value: 10,
+            discount: 1, //percent
+          },
+          {
+            label: "10X20",
+            value: 20,
+            discount: 20, //percent
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Envelopes",
+    discription:
+      "Are you ready to elevate your marketing game and capture your audience’s attention with impactful promotional materials?",
+    additionalDescription: [
+      {
+        name: "",
+        value: "",
+      },
+    ],
+    reviews: 15,
+    price: 20,
+    discountedPrice: "Upto 40% Off",
+    imgs: {
+      thumbnails: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/envelope-updated-1.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Business-Corporate-Envelopes.png",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Business-essential-Envelope.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-e-state-envelope.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-2-nd-envelope-900-X-700.jpg",
+        
+      ],
+      previews: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/envelope-updated-1.jpg",
+         "https://bjsignworld.com/wp-content/uploads/2025/02/Business-Corporate-Envelopes.png",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Business-essential-Envelope.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Real-e-state-envelope.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-2-nd-envelope-900-X-700.jpg",
+      ],
+    },
+    productOption: [
+      {
+        name: "Size",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 20,
+            discount: 0,
+          },
+          {
+            label: "10X20",
+            value: 25,
+            discount: 0,
+          },
+        ],
+      },
+      {
+        name: "Quantity",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 1,
+            discount: 0, //percent
+          },
+          {
+            label: "A4",
+            value: 10,
+            discount: 1, //percent
+          },
+          {
+            label: "10X20",
+            value: 20,
+            discount: 20, //percent
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Booklet",
+    discription:
+      "Are you ready to elevate your marketing game and capture your audience’s attention with impactful promotional materials?",
+    additionalDescription: [
+      {
+        name: "",
+        value: "",
+      },
+    ],
+    reviews: 15,
+    price: 20,
+    discountedPrice: "Upto 40% Off",
+    imgs: {
+      thumbnails: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/booklet-6-X-9-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-8.5-X-11-900-X-500.jpg",
+        
+      ],
+      previews: [
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-New.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Small-Business-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/booklet-6-X-9-900-X-500.jpg",
+        "https://bjsignworld.com/wp-content/uploads/2025/02/Booklet-8.5-X-11-900-X-500.jpg",
+      ],
+    },
+    productOption: [
+      {
+        name: "Size",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 20,
+            discount: 0,
+          },
+          {
+            label: "10X20",
+            value: 25,
+            discount: 0,
+          },
+        ],
+      },
+      {
+        name: "Quantity",
+        type: "select",
+        optionValue: [
+          {
+            label: "A4",
+            value: 1,
+            discount: 0, //percent
+          },
+          {
+            label: "A4",
+            value: 10,
+            discount: 1, //percent
+          },
+          {
+            label: "10X20",
+            value: 20,
+            discount: 20, //percent
+          },
+        ],
+      },
+    ],
+  },
+];
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // domains: ["bjsignworld.com"], // ✅ add your external image hostname
+    qualities: [75, 100],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bjsignworld.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
+        port: ""
+      }
+    ]
+  },
 };
 
 export default nextConfig;
